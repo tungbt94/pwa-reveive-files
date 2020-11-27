1fa49df7355a303fbd5c698726e9f0e48e507564
tungbt94 <tungbt94@gmail.com>
2020-11-28 01:32:38 +0700

    feat: handle share video

---
+++ pages/share/_slug.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img
+      v-if="type === 'image'"
+      :src="media.src"
+      alt="img"
+    >
+    <video
+      v-if="type === 'video'"
+      :src="media.src"
+      width="360"
+      controls
+      autoplay
+    >
+  </div>
+</template>
+
+<script>
+import { getCachedMediaMetadata } from '@@/utils/getCachedMediaMetadata'
+
+export default {
+  data() {
+    return {
+      media: '',
+      type: '',
+    }
+  },
+
+  async mounted() {
+    console.log(this.$route)
+    const mimePrefix = this.$route.params.slug
+    this.type = mimePrefix
+    const cachedMediaMetadatas = await getCachedMediaMetadata(mimePrefix)
+    this.media = cachedMediaMetadatas[0]
+  },
+}
+</script>
--- static/custom-sw.js
+++ static/custom-sw.js
@@ -1,43 +1,51 @@
 const cacheName = 'pwa-receive-files'
 const urlPrefix = '/_media/'
-const shareUrl = ['/share']
+const shareUrl = '/share'
 
-self.addEventListener('fetch', event => {
-  // if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-  //   return
-  // }
+self.addEventListener('fetch', (event) => {
+  console.log('event:', event)
+  const isCacheType = ['image', 'video'].includes(event.request.destination)
+  const loadFromCache = event.request.cache === 'default' && isCacheType
+  if (loadFromCache) {
+    return event.respondWith(caches.match(event.request))
+  }
   if (event.request.method !== 'POST') {
-    if (caches.match(event.request))
-      return event.respondWith(caches.match(event.request))
+    console.log('fetch(event.request):', fetch(event.request))
     return event.respondWith(fetch(event.request))
   }
 
-  console.log('event:', event)
+  event.respondWith(
+    (async () => {
+      const formData = await event.request.formData()
+      console.log('formData:', formData)
+      const mediaFiles = formData.getAll('media')
+      console.log('mediaFiles:', mediaFiles)
 
-  event.respondWith((async () => {
-    const formData = await event.request.formData()
-    console.log('formData:', formData)
-    const mediaFiles = formData.getAll('media')
-    console.log('mediaFiles:', mediaFiles)
+      const cache = await caches.open(cacheName)
+      for (const mediaFile of mediaFiles) {
+        if (!mediaFile.name) {
+          console.log('No media file name')
+          continue
+        }
 
-    const cache = await caches.open(cacheName)
-    for (const mediaFile of mediaFiles) {
-      if (!mediaFile.name) {
-        console.log('No media file name')
-        continue;
+        const cacheKey = new URL(
+          `${urlPrefix}${Date.now()}-${mediaFile.name}`,
+          self.location
+        ).href
+        await cache.put(
+          cacheKey,
+          new Response(mediaFile, {
+            headers: {
+              'content-length': mediaFile.size,
+              'content-type': mediaFile.type,
+            },
+          })
+        )
       }
 
-      const cacheKey = new URL(`${urlPrefix}${Date.now()}-${mediaFile.name}`, self.location).href
-      await cache.put(
-        cacheKey,
-        new Response(mediaFile, {
-          headers: {
-            'content-length': mediaFile.size,
-            'content-type': mediaFile.type,
-          },
-        })
-      )
-    }
-    return Response.redirect(shareUrl, 303)
-  })())
+      const type = mediaFiles[0].type.split('/')[0]
+      const shareEndpoint = `${shareUrl}/${type}`
+      return Response.redirect(shareEndpoint, 303)
+    })()
+  )
 })
